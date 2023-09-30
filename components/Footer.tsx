f9f2be168c98fad49bfe4443cac7243c834b6561
divarvian <divarvian@gmail.com>
2023-09-30 11:14:40 +0700

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,113 @@
+type Props = {
+  noPadding?: boolean;
+};
+
+const Footer: React.FC<Props> = ({ noPadding = false }) => {
+  return (
+    <footer
+      className={`${noPadding ? "pb-4" : "pb-24"} md:pb-4 text-center mt-auto`}
+    >
+      <div className="flex justify-center space-x-12 mb-4">
+        {socialLinks.map((social) => (
+          <a
+            key={social.id}
+            href={social.link}
+            title={social.title}
+            className="transform scale-150 md:scale-125 link-outline"
+          >
+            {social.svg}
+          </a>
+        ))}
+      </div>
+      <div>
+        Coded with <span className="sr-only">love</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 mx-1 inline-block mb-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>{" "}
+        by Diva Arviansyah
+      </div>
+    </footer>
+  );
+};
+
+const socialLinks = [
+  {
+    id: 1,
+    title: "Diva Arviansyah's Github Profile",
+    link: "https://github.com/divarvian",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        className="dark:fill-bglight hover:fill-marrsgreen dark:hover:fill-carrigreen"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 2,
+    title: "Diva Arviansyah's LinkedIn Profile",
+    link: "https://www.linkedin.com/in/divarvian/",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        className="dark:fill-bglight hover:fill-marrsgreen dark:hover:fill-carrigreen"
+      >
+        <path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM8.339 18.337H5.667v-8.59h2.672v8.59zM7.003 8.574a1.548 1.548 0 1 1 0-3.096 1.548 1.548 0 0 1 0 3.096zm11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387 2.704 0 3.203 1.778 3.203 4.092v4.71z"></path>
+      </svg>
+    ),
+  },
+  {
+    id: 3,
+    title: "Check Diva Arviansyah on Dev.to",
+    link: "https://dev.to/divarvian",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        className="dark:fill-bglight hover:fill-marrsgreen dark:hover:fill-carrigreen"
+      >
+        <path d="M7.826 10.083a.784.784 0 0 0-.468-.175h-.701v4.198h.701a.786.786 0 0 0 .469-.175c.155-.117.233-.292.233-.525v-2.798c.001-.233-.079-.408-.234-.525zM19.236 3H4.764C3.791 3 3.002 3.787 3 4.76v14.48c.002.973.791 1.76 1.764 1.76h14.473c.973 0 1.762-.787 1.764-1.76V4.76A1.765 1.765 0 0 0 19.236 3zM9.195 13.414c0 .755-.466 1.901-1.942 1.898H5.389V8.665h1.903c1.424 0 1.902 1.144 1.903 1.899v2.85zm4.045-3.562H11.1v1.544h1.309v1.188H11.1v1.543h2.142v1.188h-2.498a.813.813 0 0 1-.833-.792V9.497a.813.813 0 0 1 .792-.832h2.539l-.002 1.187zm4.165 4.632c-.531 1.235-1.481.99-1.906 0l-1.548-5.818h1.309l1.193 4.569 1.188-4.569h1.31l-1.546 5.818z"></path>
+      </svg>
+    ),
+  },
+  {
+    id: 4,
+    title: "Diva Arviansyah's Profile on instagram",
+    link: "https://www.instagram.com/divarvian",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        className="dark:fill-bglight hover:fill-marrsgreen dark:hover:fill-carrigreen"
+      >
+        <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"/>
+      </svg>
+    ),
+  },
+];
+
+export default Footer;
